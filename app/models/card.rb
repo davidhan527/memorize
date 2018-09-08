be8dc9ffba66bb1281d6c2133008bcb067920a18
David Han <davidhan527@gmail.com>
2018-09-08 14:26:43 -0400

    change to rails_admin

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,10 @@
 class Card < ApplicationRecord
   has_paper_trail class_name: 'CardVersion'
   belongs_to :user
+
+  rails_admin do
+    configure :user do
+      label 'Owner of this card: '
+    end
+  end
 end
